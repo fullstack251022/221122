157339b78315133082bc2c05c167c69be08c57f8
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-22 21:25:17 +0200

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,151 @@
+public class Main {
+//    static void printHelloWorld() {
+//        System.out.println("Hello friends");
+//    }
+//
+//    static void printSumOfTwoNumbers(int x, int y) {
+//        System.out.println(x + y);
+//    }
+//
+//    static int printAndReturnSumOfTwoNumbers(int x, int y) {
+//        int sum = x + y;
+//        System.out.println(sum);
+//        return (sum);
+//    }
+//
+//    static void sayHelloToYourName(String name) {
+//        System.out.println("Hello " + name);
+//    }
+
+
+//    static void printAgeAndName(String s, int x) {
+//        System.out.println(s + " is a " + x + " years old");
+//    }
+
+
+//    static int multiplyTwoNumbers(int a, int b){
+//        return a*b;
+//    }
+
+//    static boolean checkIfOverEighteenYearsOld(int age) {
+//        if (age >= 18) {
+//            System.out.println("You are over 18 years old ");
+//            return true;
+//        } else {
+//            System.out.println("You are to small");
+//            return false;
+//        }
+//    }
+
+    static void printHelloToUser() {
+        System.out.println("Hello User");
+    }
+
+    static void printHelloToUser(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    static void printHelloToUser(int age) {
+        System.out.println("Hello you are " + age + " years old");
+    }
+
+    static void printHelloToUser(String name, int age) {
+        System.out.println("Hello " + name + " and you are " + age + " years old");
+    }
+
+    static void printHelloToUser(int age, int ageTwo) {
+        System.out.println("your first age is " + age + " and your second age is " + ageTwo);
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+        int x = 50;
+
+        int c = 10 + x++;
+        System.out.println(c);
+        System.out.println(x);
+
+//        printHelloToUser();
+//
+//        printHelloToUser("beni");
+//
+//        printHelloToUser(50);
+//
+//        printHelloToUser("beni", 50);
+//
+//        printHelloToUser(50, 50);
+
+
+//        int age = 15;
+//        boolean isAllowedToWatchMovie = checkIfOverEighteenYearsOld(age);
+////        System.out.println(isOlder);
+//        if(isAllowedToWatchMovie){
+////            showMovie();
+//        }else{
+////            sendUserToLogout();
+//        }
+
+//        int multiplyResult = multiplyTwoNumbers(5,10);
+//        System.out.println(multiplyResult);
+
+
+        //create a method that accepts two parameters(int) and returns their multiply (Kefel')
+
+
+//        int numOne = 5;
+//        int numTwo = 10;
+//
+//        int x = Math.min(numOne, numTwo);
+//        System.out.println(x);
+////        System.out.println(Math.min(numOne, numTwo));
+//
+//
+//        String name = "Beni";
+//        int age = 18;
+//        printAgeAndName(name, age);
+//        printAgeAndName("Sarah", 15);
+//        printAgeAndName("Michel", 50);
+
+
+//        printSumOfTwoNumbers(5,10);
+//        printHelloWorld();
+
+//        int num = 5 + 5;
+//        int x = printAndReturnSumOfTwoNumbers(5,10);
+
+
+//             int sum =x+ 10 ;
+
+
+//         int x = Math.min(10,20);
+//             System.out.println(5);
+
+
+//        sayHelloToYourName("Beni");
+//        sayHelloToYourName("Sarah");
+//        sayHelloToYourName("Ryu");
+
+
+//        printHelloWorld();
+//        printHelloWorld();
+//        printHelloWorld();
+//        printHelloWorld();
+//        printHelloWorld();
+
+//        printSumOfTwoNumbers(5 , 10);
+//        printSumOfTwoNumbers(10 , 10);
+//        printSumOfTwoNumbers(20 , 30);
+//        printSumOfTwoNumbers(40 , 10);
+//        printSumOfTwoNumbers(a2);
+
+        //create a method that accept your name (String)
+        //The method should print Hello +{Your Name}
+        //for example if I pass Beni to the method it should print "Hello Beni"
+
+    }
+
+
+}
